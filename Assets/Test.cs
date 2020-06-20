e7a50090bd569588217a68225326daac14d2d90e
804052545@qq.com <804052545@qq.com>
2020-06-20 14:38:25 +0800

    第一次提交

---
+++ Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿/****************************************************************
+ *Author:       #AUTHOR# 
+ *UnityVersion：#UNITYVERSION# 
+ *Date:         #DATE# 
+ *Description:    
+*****************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    private string str = "First";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
